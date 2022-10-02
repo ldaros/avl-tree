74d67faed26262bc3181da3c372f738cb1709912
Luan Daros <darosluan@gmail.com>
2022-10-02 14:22:14 -0300

    remove function

--- avl/src/lib.rs
+++ avl/src/lib.rs
@@ -19,7 +19,19 @@ mod tests {
         assert!(!tree.is_empty());
     }
 
-    // Should be able to insert multiple elements into an empty tree.
+    /// Should be able to search for an element in a tree.
+    #[test]
+    fn test_search() {
+        let mut tree: Tree<i32> = Tree::new();
+        tree.insert(1);
+        tree.insert(2);
+        tree.insert(3);
+
+        assert_eq!(tree.search(3), Some(3));
+        assert_eq!(tree.search(4), None);
+    }
+
+    /// Should be able to insert multiple elements into an empty tree.
     #[test]
     fn test_insert_multiple() {
         let mut tree: Tree<i32> = Tree::new();
@@ -29,7 +41,7 @@ mod tests {
         assert!(!tree.is_empty());
     }
 
-    // Should return an vector of elements in order.
+    /// Should return an vector of elements in order.
     #[test]
     fn test_in_order() {
         let mut tree: Tree<i32> = Tree::new();
@@ -39,9 +51,20 @@ mod tests {
         assert_eq!(vec![1, 2, 3], tree.in_order());
     }
 
-    // should perform a left rotation
+    /// Should return an vector of elements in post order.
     #[test]
-    fn test_insert_balance() {
+    fn test_post_order() {
+        let mut tree: Tree<i32> = Tree::new();
+        tree.insert(3);
+        tree.insert(1);
+        tree.insert(2);
+        tree.insert(4);
+        assert_eq!(vec![1, 4, 3, 2], tree.post_order());
+    }
+
+    /// Should perform a left rotation.
+    #[test]
+    fn test_insert_balance_ll() {
         let mut tree: Tree<i32> = Tree::new();
         tree.insert(1);
         tree.insert(2);
@@ -50,9 +73,9 @@ mod tests {
         assert_eq!(vec![2, 1, 3], tree.pre_order());
     }
 
-    // should perform a right rotation
+    /// Should perform a right rotation.
     #[test]
-    fn test_insert_balance2() {
+    fn test_insert_balance_rr() {
         let mut tree: Tree<i32> = Tree::new();
         tree.insert(3);
         tree.insert(2);
@@ -61,9 +84,9 @@ mod tests {
         assert_eq!(vec![2, 1, 3], tree.pre_order());
     }
 
-    // should perform a left-right rotation
+    /// Should perform a left-right rotation.
     #[test]
-    fn test_insert_balance3() {
+    fn test_insert_balance_lr() {
         let mut tree: Tree<i32> = Tree::new();
         tree.insert(3);
         tree.insert(1);
@@ -72,15 +95,31 @@ mod tests {
         assert_eq!(vec![2, 1, 3], tree.pre_order());
     }
 
-    // should perform a right-left rotation
+    /// Should perform a right-left rotation.
     #[test]
-    fn test_insert_balance4() {
+    fn test_insert_balance_rl() {
         let mut tree: Tree<i32> = Tree::new();
         tree.insert(1);
         tree.insert(3);
         tree.insert(2);
 
         assert_eq!(vec![2, 1, 3], tree.pre_order());
+    }
+
+    /// Should be able to remove an element from a tree.
+    #[test]
+    fn test_removes() {
+        let mut tree: Tree<i32> = Tree::new();
+        tree.insert(6);
+        tree.insert(3);
+        tree.insert(4);
+        tree.insert(1);
+        tree.insert(2);
+        tree.insert(5);
+
+        tree.remove(4);
+
+        assert_eq!(vec![3, 2, 1, 6, 5], tree.pre_order());
     }
 }
 
@@ -90,31 +129,62 @@ pub struct Tree<T: Ord> {
 }
 
 impl<T: Ord + Clone> Tree<T> {
-    /// Create a new AVL tree with an empty root.
+    /// Creates a new AVL tree with an empty root.
     pub fn new() -> Tree<T> {
         Tree { root: None }
     }
 
-    /// Check if the tree is empty.
+    /// Checks if the tree is empty.
     pub fn is_empty(&self) -> bool {
         self.root.is_none()
     }
 
-    /// Insert a new element into the tree.
-    pub fn insert(&mut self, value: T) {
-        self.root = insert_recursive(self.root.take(), value);
+    /// Returns the first element with the given key.
+    pub fn search(&self, key: T) -> Option<T> {
+        search_recursively(&self.root, key)
     }
 
+    /// Inserts a new element into the tree.
+    pub fn insert(&mut self, value: T) {
+        self.root = insert_recursively(self.root.take(), value);
+    }
+
+    /// Removes the first element with the given key.
+    pub fn remove(&mut self, key: T) {
+        self.root = remove_recursively(self.root.take(), key);
+    }
+
+    /// Returns a vector of elements in order.
     pub fn in_order(&self) -> Vec<T> {
         let mut result = Vec::new();
         in_order_recursive(&self.root, &mut result);
         result
     }
 
+    /// Returns a vector of elements in pre-order.
     pub fn pre_order(&self) -> Vec<T> {
         let mut result = Vec::new();
         pre_order_recursive(&self.root, &mut result);
         result
+    }
+
+    /// Returns a vector of elements in post-order.
+    pub fn post_order(&self) -> Vec<T> {
+        let mut result = Vec::new();
+        post_order_recursive(&self.root, &mut result);
+        result
+    }
+}
+
+fn search_recursively<T: Ord + Clone>(node: &Option<Box<Node<T>>>, value: T) -> Option<T> {
+    match *node {
+        None => None,
+
+        Some(ref n) => match value.cmp(&n.key) {
+            Ordering::Equal => Some(n.key.clone()),
+            Ordering::Less => search_recursively(&n.left, value),
+            Ordering::Greater => search_recursively(&n.right, value),
+        },
     }
 }
 
@@ -134,13 +204,58 @@ fn pre_order_recursive<T: Ord + Clone>(node: &Option<Box<Node<T>>>, result: &mut
     }
 }
 
-fn insert_recursive<T: Ord + Clone>(root: Option<Box<Node<T>>>, value: T) -> Option<Box<Node<T>>> {
+fn post_order_recursive<T: Ord + Clone>(node: &Option<Box<Node<T>>>, result: &mut Vec<T>) {
+    if let Some(ref n) = *node {
+        post_order_recursive(&n.left, result);
+        post_order_recursive(&n.right, result);
+        result.push(n.key.clone());
+    }
+}
+
+fn remove_recursively<T: Ord + Clone>(
+    node: Option<Box<Node<T>>>,
+    value: T,
+) -> Option<Box<Node<T>>> {
+    match node {
+        None => None,
+
+        Some(mut n) => match value.cmp(&n.key) {
+            Ordering::Equal => {
+                if n.left.is_none() {
+                    n.right
+                } else if n.right.is_none() {
+                    n.left
+                } else {
+                    let mut temp = largest_value(&n.left);
+                    n.key = temp.take().unwrap();
+                    n.left = remove_recursively(n.left, n.key.clone());
+                    Some(n)
+                }
+            }
+
+            Ordering::Less => {
+                n.left = remove_recursively(n.left, value);
+                Some(n)
+            }
+
+            Ordering::Greater => {
+                n.right = remove_recursively(n.right, value);
+                Some(n)
+            }
+        },
+    }
+}
+
+fn insert_recursively<T: Ord + Clone>(
+    root: Option<Box<Node<T>>>,
+    value: T,
+) -> Option<Box<Node<T>>> {
     match root {
         None => Some(Box::new(Node::new(value))),
 
         Some(mut node) => match value.cmp(&node.key) {
             Ordering::Less | Ordering::Equal => {
-                node.left = insert_recursive(node.left, value.clone());
+                node.left = insert_recursively(node.left, value.clone());
 
                 if should_balance(&node.left, &node.right) {
                     if value < node.left.as_ref().unwrap().key {
@@ -155,7 +270,7 @@ fn insert_recursive<T: Ord + Clone>(root: Option<Box<Node<T>>>, value: T) -> Opt
             }
 
             Ordering::Greater => {
-                node.right = insert_recursive(node.right, value.clone());
+                node.right = insert_recursively(node.right, value.clone());
 
                 if should_balance(&node.right, &node.left) {
                     if value > node.right.as_ref().unwrap().key {
@@ -168,6 +283,17 @@ fn insert_recursive<T: Ord + Clone>(root: Option<Box<Node<T>>>, value: T) -> Opt
                 update_height(&mut node);
                 Some(node)
             }
+        },
+    }
+}
+
+fn largest_value<T: Ord + Clone>(node: &Option<Box<Node<T>>>) -> Option<T> {
+    match node {
+        None => None,
+
+        Some(ref n) => match n.right {
+            None => Some(n.key.clone()),
+            Some(ref r) => largest_value(&Some(r.clone())),
         },
     }
 }
