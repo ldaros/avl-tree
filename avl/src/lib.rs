b6c6b07db2751cfb7911363ffd5a2f1de2b3499e
Luan Daros <darosluan@gmail.com>
2022-10-01 22:18:14 -0300

    using generics

--- avl/src/lib.rs
+++ avl/src/lib.rs
@@ -1,39 +1,53 @@
+use std::cmp::max;
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    /// Should be able to create a new AVL tree with no elements.
     #[test]
     fn test_new() {
-        let tree = Tree::new();
+        let tree: Tree<i32> = Tree::new();
         assert!(tree.is_empty());
     }
 }
 
-pub struct Tree {
-    root: Option<Box<Node>>,
+#[derive(Debug, PartialEq, Clone)]
+pub struct Tree<T: Ord> {
+    root: Option<Box<Node<T>>>,
 }
 
-impl Tree {
-    pub fn new() -> Tree {
+impl<T: Ord> Tree<T> {
+    /// Create a new AVL tree.
+    pub fn new() -> Tree<T> {
         Tree { root: None }
     }
 
+    /// Check if the tree is empty.
     pub fn is_empty(&self) -> bool {
         self.root.is_none()
     }
 }
 
-struct Node {
-    key: i32,
-    height: i32,
-    left: Option<Box<Node>>,
-    right: Option<Box<Node>>,
+fn height<T: Ord>(node: &Option<Box<Node<T>>>) -> i32 {
+    match *node {
+        Some(ref n) => n.height,
+        None => 0,
+    }
 }
 
-impl Node {
-    pub fn new(key: i32) -> Node {
+#[derive(Debug, PartialEq, Clone)]
+struct Node<T: Ord> {
+    key: T,
+    height: i32,
+    left: Option<Box<Node<T>>>,
+    right: Option<Box<Node<T>>>,
+}
+
+impl<T: Ord> Node<T> {
+    fn new(key: T) -> Self {
         Node {
-            key: key,
+            key,
             height: 0,
             left: None,
             right: None,
